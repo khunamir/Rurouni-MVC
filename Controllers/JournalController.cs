9b435e72eec37a0c178385397882dc0376849926
khunamir <amir12102009@gmail.com>
2021-02-24 21:54:23 +0800

    Journal Index

---
+++ Controllers/JournalController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rurouni_v2.Data;
+using Rurouni_v2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rurouni_v2.Controllers
+{
+    public class JournalController : Controller
+    {
+        private readonly JournalDbContext _db;
+
+        public JournalController(JournalDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<JournalModel> objList = _db.Journals;
+            return View(objList);
+        }
+    }
+}
